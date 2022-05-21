fd5fad87ae15f7cd6cda097c798f6dfd39025c09
bahram4454 <bahram4454@gmail.com>
2022-05-21 13:50:57 +0430

    responsive 1

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".header__icon")
+let menuBtnIcon = document.querySelector(".header__icon i")
+
+
+menuBtn.addEventListener("click",function (){
+    if (menuBtnIcon.classList.contains("fa-bars")){
+        menu.style.left = "0"
+        menuBtnIcon.classList = "fa fa-times"
+        menuBtn.style.position = "fixed"
+        menuBtn.style.right = "1rem"
+
+    } else {
+        menu.style.left = "-256px"
+        menuBtnIcon.classList = "fa fa-bars"
+        menuBtn.style.position = "static"
+    }
+})
+
+function hideMenu(){
+    menu.style.left = "-256px"
+    menuBtnIcon.classList = "fa fa-bars"
+    menuBtn.style.position = "static"
+
+}
